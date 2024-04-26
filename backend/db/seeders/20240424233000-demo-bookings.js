a1f3f94cab57401b822b4e5e6e539ef4ccc9efa3
harmony <harmony.rosenberg94@gmail.com>
2024-04-25 19:05:16 -0500

    added options objects to all seeders

--- backend/db/seeders/20240424233000-demo-bookings.js
+++ backend/db/seeders/20240424233000-demo-bookings.js
@@ -2,6 +2,11 @@
 
 const { Booking } = require('../models');
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -24,11 +29,12 @@ module.exports = {
       startDate: '2024-06-27',
       endDate: '2024-06-29'
     },
-   ])
+   ], options, { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Bookings', {
+    options.tableName = 'Bookings'
+    return queryInterface.bulkDelete(options, {
       spotId: 1,
       userId: 2,
       startDate: '2024-04-27',

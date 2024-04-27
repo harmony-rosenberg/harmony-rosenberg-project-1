24dab575ac2cfe3025f5ddf90a6ed214ab94d98f
harmony <harmony.rosenberg94@gmail.com>
2024-04-27 14:38:52 -0500

    completed get reviews for current user router

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { Spot, spotImage, Review, User, reviewImage} = require('../../db/models');
+
+router.get('/current', async(req, res, next) => {
+	const userReviews = await Review.findAll({
+		where: {
+			userId: req.user.id
+		}, include : [{
+			model: User,
+			attributes: ['id', 'firstName', 'lastName']
+		},
+		{
+			model: Spot,
+			attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+			include: {
+				model: spotImage,
+				attributes: ['url']
+			}
+		}, {
+			model: reviewImage,
+			attributes: ['id', 'url']
+		}]
+	})
+
+	res.json(userReviews)
+})
+
+
+module.exports = router;

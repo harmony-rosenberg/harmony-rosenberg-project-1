4e9bc5a54a0623bd4edcf884684d5fd4c328e387
harmony <harmony.rosenberg94@gmail.com>
2024-04-26 23:14:54 -0500

    added get all, get current, create, and delete spots routers IN PROGRESS

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,19 +1,79 @@
 const express = require('express');
 const router = express.Router();
-const { Spot } = require('../../db/models')
-const { spotImages } = require('../../db/models')
+const { Spot, spotImage } = require('../../db/models')
 
 //GET ALL SPOTS
 router.get('/', async (req, res) => {
 
-	let spots = await Spot.findAll()
+	// let spots = await Spot.findAll({include: spotImage})
 
-	// const images = await spots.getspotImages()
+	let spots = spotImage.findAll({include: Spot})
 
-	// console.log('test!!!!!!!!!!!!!!!!!!', images)
-
+	// console.log('test!!!!!!!!!!!!!!!!!!', spots[0].spotImage)
 
 	res.json(spots)
 })
+
+//GET ALL SPOTS FOR CURRENT USER
+router.get('/current', async (req, res, next) => {
+
+})
+
+
+//CREATE A SPOT
+router.post('/', async (req, res, next) => {
+		try {
+		const {ownerId, address, city, state, country, lat, lng, name, description, price} = req.body
+
+		const newSpot = await Spot.create({
+			ownerId,
+			address,
+			city,
+			state,
+			country,
+			lat,
+			lng,
+			name,
+			description,
+			price
+		});
+
+		await newSpot.validate();
+
+		res.json({
+			message: "success!",
+			data: newSpot
+		})
+
+	} catch(err) {
+		next({
+			status: 404,
+			message: "you messed something up, buddy!"
+		})
+	}
+
+})
+
+//DELETE A SPOT BY ID
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const deadSpot = await Spot.findOne({where: {id: req.params.id}});
+
+		await deadSpot.destroy();
+
+		res.json({
+			status: "success!",
+			message: `we killed your spot ${req.params.id}`
+		})
+
+		} catch(err) {
+			next({
+				status: 404,
+				message: "doesnt look like this spot exists, buddy"
+			})
+		}
+	})
+
+
 
 module.exports = router;

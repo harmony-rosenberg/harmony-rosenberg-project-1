4dbe0e04ccb8b57140c4c7e80508ccc8fa79eaa0
harmony <harmony.rosenberg94@gmail.com>
2024-06-03 18:42:31 -0500

    implemented login modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+// import LoginFormPage from './components/LoginFormPage/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
@@ -31,10 +31,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />
       },
       {
         path: "signup",
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/ store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { ModalProvider, Modal } from './context/Modal';
 
 const store = configureStore();
 
@@ -24,8 +25,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ModalProvider>
     <Provider store={store}>
       <App />
+      <Modal />
     </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );

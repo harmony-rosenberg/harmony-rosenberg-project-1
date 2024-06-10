13d38621a74c328271ce57d8dfd92b02cb991c7e
harmony <harmony.rosenberg94@gmail.com>
2024-06-10 10:17:06 -0500

    formatted review modal

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -3,13 +3,20 @@ import './ReviewForm.css';
 const ReviewFormModal = () => {
 	return (
 		<main>
-		<form>
+		<form className='review-form'>
+			<h1>How was your stay?</h1>
 			<label>
-				<input
-				placeholder='yeeehaw'
+				<textarea
+				cols={50}
+				rows={10}
+				placeholder='Leave your review here...'
 				type='text'
 				/>
 			</label>
+			<label>
+				<div>Stars</div>
+			</label>
+			<button className='submit-btn' type='submit'>Submit Your Review</button>
 		</form>
 		</main>
 	)
